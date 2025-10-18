d2f640ae0e2a4ad3ea3d7e405ec44b4bb277548a
Lrh <2769611058@qq.com>
2025-10-18 12:51:21 +0800

    step-8.7

--- 331-intro-to-vite/src/views/HomeView.vue
+++ 331-intro-to-vite/src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import EventCard from '@/components/EventCard.vue'
+import EventSummary from '@/components/EventSummary.vue'
 import type { Event } from '@/types'
 import { ref } from 'vue'
 const events = ref<Event[]>([
@@ -40,7 +41,16 @@ const events = ref<Event[]>([
 </script>
 
 <template>
-  <div class="home">
+  <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <EventSummary v-for="event in events" :key="`summary-${event.id}`" :event="event" />
   </div>
 </template>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
